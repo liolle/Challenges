83963c0bb401a936b09e4544a780875bae1df4df
liolle <laurentlionel20@hotmail.com>
2022-11-14 19:11:55 +0100

    Update list & localStorage

--- todo-app-master/script.js
+++ todo-app-master/script.js
@@ -1,10 +1,30 @@
 const input = document.querySelectorAll('input[type="checkbox"]');
 const li = document.querySelectorAll('.list-items ul li');
-const ul = document.querySelector('.list-items ul');
+const uls = document.querySelectorAll('.list-items ul');
 
 var task_input = document.querySelector('.add-bar-container input');
 
 const add_button = document.querySelector('.add-bar-container button');
+
+const add_bar = document.querySelector('.add-bar');
+
+
+class Tasck {
+
+    constructor(name, statu) {
+        this.name = name;
+        this.statu = statu;
+    }
+
+    toString(){
+        return `${this.name},${this.statu}`;
+    }
+
+    static creatFromString(str){
+        let split_string = str.split(',');
+        return new Tasck(split_string[0],split_string[1]);
+    }
+}
 
 
 for (let index = 0; index < input.length; index++) {
@@ -21,16 +41,48 @@ for (let index = 0; index < input.length; index++) {
     
 }
 
+function addLocal(task){
 
- function addTasck(tasck) {
-    var ul = document.querySelector('.list-items ul');
+    for(let char of task){
+        if (char === ','){
+            alert("Please remove all commas from the tasck name");
+            return;
+        }
+    }
+
+    let size = localStorage.length;
+
+    // state : "done" | "todo"
+
+    const new_tasck = new Tasck(task,"todo");
+
+    var key ;
+
+    for (let index = 0; index < 5 && localStorage.length<=size; index++) {
+        let random_num = Math.floor(Math.random()*10000)
+        key = random_num;
+        localStorage.setItem(random_num,new_tasck);
+        
+    }
+
+    //trigger storage event 
+    var evt = new StorageEvent('storage', {
+        key: key,
+        newValue: new_tasck
+    });
+
+    dispatchEvent(evt);
+    task_input.value = "";
+}
+
+function addLi(taskName,ul,statu){
+
     var li = document.createElement("li");
-    var inp = document.createElement("INPUT");
-    inp.setAttribute("type","checkbox");
+    var inp = document.createElement("input");
 
+    inp.setAttribute("type","checkbox");
     li.appendChild(inp);
-    li.appendChild(document.createTextNode(tasck));
-    ul.appendChild(li);
+    li.appendChild(document.createTextNode(taskName));
 
     inp.addEventListener("change",()=>{
         if (inp.checked){
@@ -42,6 +94,39 @@ for (let index = 0; index < input.length; index++) {
             li.style.color = "black";
         }
     });
+
+    if(statu === "done"){
+        inp.click();
+    }
+    ul.appendChild(li);
+
+
+}
+
+
+ function addTasck(task,key) {
+
+
+    //var ul = document.querySelector('.list-items ul');
+
+    task_split = task.split(',');
+
+
+    switch (task_split[1]){
+        case "todo":
+            addLi(task_split[0],uls[0],"todo");
+            addLi(task_split[0],uls[1],"todo");
+            break;
+
+        case "done":
+            addLi(task_split[0],uls[0],"done");
+            addLi(task_split[0],uls[2],"done");
+            break;
+        default:
+            break;
+    }
+    
+    
 }
 
 
@@ -53,9 +138,8 @@ add_button.addEventListener("click",()=>{
 
         //add tasck in list
 
-        addTasck(tasck);
+        addLocal(tasck);
 
-        task_input.value = "";
     }
 });
 
@@ -79,7 +163,33 @@ for (let index = 0; index < marker_list.length; index++) {
             let current_list = document.querySelector('.active_list'); 
             current_list.classList.remove("active_list");
             lists[index].classList.add("active_list");
+
+            if (index == 2){
+                console.log("hidden");
+                add_bar.classList.add("hidden");
+
+            }
+            else {
+                add_bar.classList.remove("hidden");
+            }
+            
         }
     });
     
+}
+
+window.addEventListener('storage', (e)=>{
+    console.log(e)
+    if (e.newValue != null){
+        let key = parseInt(e.key);
+        addTasck(e.newValue,key);
+    }
+
+}, false);
+
+/* Populate list (on page load) */
+
+for (let index = 0; index < localStorage.length; index++) {
+    let key = localStorage.key(index);
+    addTasck(localStorage.getItem(key),key);    
 }
